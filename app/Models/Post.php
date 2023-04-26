4f3013f3ecf35648df02cf0448aeb31dcbfc4647
rezarubik <muhammad.reza.pahlevi.y@gmail.com>
2023-04-26 14:49:40 +0700

    Fixing query search when

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -28,10 +28,13 @@ class Post extends Model
         // }
 
         // todo: use when
-        $query->when($filters['search'] ?? false, function ($query, $search) {
-            return $query->where('title', 'like', '%' . $search . '%')
-                ->orWhere('body', 'like', '%' . $search . '%');
-        });
+        $query->when($filters['search'] ?? false, function($query, $search) {
+            return $query->where(function($query) use ($search) {
+                 $query->where('title', 'like', '%' . $search . '%')
+                             ->orWhere('body', 'like', '%' . $search . '%');
+             });
+         });
+ 
 
         // todo: when searchnya ada category, versi callback
         $query->when($filters['category'] ?? false, function ($query, $category) {
